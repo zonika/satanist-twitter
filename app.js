1e83824a75e3b6dc84550179da956f5e8e8fbe00
zonika <zchodosh@gmail.com>
2016-06-27 22:12:19 -0400

    shit for heroku

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-var config = require('./config.json'),
+var config = {
+  'userId': process.env.USER_ID,
+  'api': {
+    'consumer_key': process.env.CONSUMER_KEY,
+    'consumer_secret': process.env.CONSUMER_SECRET,
+    'access_token_key': process.env.ACCESS_TOKEN_KEY,
+    'access_token_secret': process.env.ACCESS_TOKEN_SECRET
+  }
+},
     Twitter = require('twitter'),
     Promise = require('bluebird'),
     jsonfile = require('jsonfile'),
